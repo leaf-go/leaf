327ab274ba47b93a411d8759b28f5912659a1308
李烽 <leaf_go@126.com>
2022-05-12 18:17:06 +0800

    x

--- apps/http/middleware/init.go
+++ apps/http/middleware/init.go
@@ -2,17 +2,23 @@ package middleware
 
 import (
 	"github.com/gin-gonic/gin"
-	"leaf/mounts"
+	"github.com/leaf-go/x"
+	"time"
 )
 
+// Init 初始化
 func Init() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// 初始化日志
-		ctx.Set("logger", mounts.Logger())
+		// 绑定日志
+		bindLogger(ctx)
 
-
-
+		// 获取时间
+		ctx.Set("start_at", time.Now())
 	}
 }
 
-
+// 绑定日志
+func bindLogger(ctx *gin.Context) {
+	log := x.NewLogger(ctx.Request.Method, ctx.Request.RequestURI, ctx.ClientIP())
+	ctx.Set("log", log)
+}
